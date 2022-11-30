af5d32f834a51bc9a56f5f1fef2736af6f10eb5d
Othkkartho <kimwonkyu15@gmail.com>
2022-11-30 16:23:57 +0900

    Selector를 기반으로 동작하는 채팅 프로그램의 클라이언트

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,41 +1,40 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 
 public class ChatClient {
-    public ChatClient() {
-        SocketAddress address = new InetSocketAddress("127.0.0.1", 5001);
-        try (SocketChannel socketChannel = SocketChannel.open(address)) {
-            System.out.println("Connected to Chat Server");
-            String message;
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                // Receive message
-                System.out.println("Waiting for message from the server ...");
-                System.out.println("Message: " + HelperMethods.receiveMessage(socketChannel));
-//                System.out.println("Message: "
-//                        + HelperMethods.receiveFixedLengthMessage(
-//                                socketChannel));
-                System.out.print("> ");
-                message = scanner.nextLine();
-                if (message.equalsIgnoreCase("quit")) {
-                    HelperMethods.sendMessage(socketChannel, "Client terminating");
-//                HelperMethods.sendFixedLengthMessage(
-//                        socketChannel, "Client terminating");
-                    break;
-                }
-                // Send message
-                HelperMethods.sendMessage(socketChannel, message);
-//                HelperMethods.sendFixedLengthMessage(socketChannel, message);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        new ChatClient();
+        Scanner scn = new Scanner(System.in);
+        SocketChannel client;
+        //ByteBuffer buffer;
+
+        try {
+            client = SocketChannel.open(new InetSocketAddress("localhost", 5454));
+            //buffer = ByteBuffer.allocate(256);
+
+            while(true) {
+                System.out.print("message : ");
+                String msg = scn.nextLine();
+                ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+                String response = null;
+
+                try {
+                    client.write(buffer);
+                    if(msg.equals("quit")) break;
+                    buffer.clear();
+                    client.read(buffer);
+                    response = new String(buffer.array()).trim();
+                    System.out.println("response = " + response);
+                    buffer.clear();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
